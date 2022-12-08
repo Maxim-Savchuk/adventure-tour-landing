f65900e4573d4cb4617876f95c3f9b6bbcb86bbc
LENOVO <maxsavchuk1105@gmail.com>
2022-12-08 22:07:28 +0200

    Finish header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// -------- Header-menu --------
+
 let navbar = document.querySelector('.header__navbar');
 
 document.querySelector('#menu-btn').addEventListener('click', () => {
@@ -10,4 +12,16 @@ document.querySelector('#nav-close').addEventListener('click', () => {
 
 window.addEventListener('scroll', () => {
   navbar.classList.remove('active');
+});
+
+// -------- Header-search ----------
+
+let searchForm = document.querySelector('.header__searchform');
+
+document.querySelector('#search-btn').addEventListener('click', () => {
+  searchForm.classList.add('active');
+});
+
+document.querySelector('#close-search').addEventListener('click', () => {
+  searchForm.classList.remove('active');
 });
