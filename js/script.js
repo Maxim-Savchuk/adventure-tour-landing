1c4449fb8dd141ac6a71001f609503db7fa91c98
LENOVO <maxsavchuk1105@gmail.com>
2022-12-10 23:53:42 +0200

    Finished shop section

--- js/script.js
+++ js/script.js
@@ -50,3 +50,27 @@ var swiper = new Swiper('.home__slider', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+var swiper = new Swiper('.shop__slider', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
